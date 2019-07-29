69ce9185cc3f43a33f8dd12a8aee08aa56cd206a
Ilya Omelyukhin <i.omelyuhin@gmail.com>
2019-07-30 02:31:47 +0300

    add login

--- src/admin/requests.js
+++ src/admin/requests.js
@@ -6,6 +6,24 @@ axios.defaults.baseURL = "https://webdev-api.loftschool.com/";
 
 axios.defaults.headers["Authorization"] = `Bearer ${token}`;
 
-//axios.get('/categories/161').then(function (response) { console.log(response);})
+axios.interceptors.response.use(
+  function(response) {
+    return response;
+  },
+  error => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401) {
+      return axios.post("/refreshToken").then(response => {
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+        axios.defaults.headers["Authorization"] = `Bearer ${token}`;
+        originalRequest.headers["Authorization"] = `Bearer ${token}`;
+        return axios(originalRequest);
+      });
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axios;
