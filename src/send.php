a29ddb54052c3c25a36a94266ba55880fb8c2735
Ilya Omelyukhin <i.omelyuhin@gmail.com>
2019-08-22 16:03:23 +0300

    finish

---
+++ src/send.php
@@ -0,0 +1,21 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$name = htmlspecialchars($name);
+$email = htmlspecialchars($email);
+$message = htmlspecialchars($message);
+$name = urldecode($name);
+$email = urldecode($email);
+$message = urldecode($message);
+$name = trim($name);
+$email = trim($email);
+$message = trim($message);
+//echo $name;
+//echo "<br>";
+//echo $email;
+if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$name.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено"; 
+} else { 
+    echo "при отправке сообщения возникли ошибки";
+}?>
