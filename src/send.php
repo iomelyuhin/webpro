4cde435e8f9d1884ac8d1d1773a254aaeeb071ab
Ilya Omelyukhin <i.omelyuhin@gmail.com>
2019-08-22 16:35:25 +0300

    finish

--- src/send.php
+++ src/send.php
@@ -14,7 +14,7 @@ $message = trim($message);
 //echo $name;
 //echo "<br>";
 //echo $email;
-if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$name.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
+if (mail("i.omelyuhin@gmail.com", "Заявка с сайта", "ФИО:".$name.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
  {     echo "сообщение успешно отправлено"; 
 } else { 
     echo "при отправке сообщения возникли ошибки";
